f0c63c770f7a16a30a5d64e7f185eb3a6e8864f4
Sebastian L <sebastian.luque.pl@gmail.com>
2026-03-16 10:03:29 -0500

    1.0.11

--- wxt.config.ts
+++ wxt.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
     name: "Downloader for Reddit - RedditGrab",
     description:
       "Download images and videos from Reddit posts (including Redgifs)",
-    version: "1.0.10",
+    version: "1.0.11",
     permissions: [
       "downloads",
       "activeTab",
